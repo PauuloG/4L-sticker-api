aba46e6009bd96ca59cf043831cd9dd0fa9339c2
PauuloG <paulgabriel7@gmail.com>
2016-02-07 01:22:52 +0100

    Revert IPN Fail

--- src/AppBundle/Controller/DonationController.php
+++ src/AppBundle/Controller/DonationController.php
@@ -9,7 +9,8 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 use AppBundle\Entity\Donation;
 use AppBundle\Form\DonationType;
-use Payum\Paypal\Ipn\Api;
+
+//bonnjour je suis sur la branche
 
 /**
  * Donation controller.
@@ -28,10 +29,10 @@ class DonationController extends Controller
 
         $donations = $em->getRepository('AppBundle:Donation')->findBy(['printed' => 0]);
 
-        foreach ($donations as $donation) {
-            $donation->setPrinted(1);
-            $em->persist($donation);
-        }
+        // foreach ($donations as $donation) {
+        //     $donation->setPrinted(1);
+        //     $em->persist($donation);
+        // }
 
         $data['containers'] = [];
 
@@ -47,9 +48,18 @@ class DonationController extends Controller
 
         }
 
-        $em->flush();
+        // $em->flush();
 
         $html = $this->renderView('sticker.html.twig', $data);
+
+        // return new Response(
+        //     $this->get('knp_snappy.pdf')->getOutputFromHtml($html),
+        //     200,
+        //     array(
+        //         'Content-Type'          => 'application/pdf',
+        //         'Content-Disposition'   => 'attachment; filename="file.pdf"',
+        //     )
+        // );
 
         return $this->render('sticker.html.twig', $data);
 
@@ -62,126 +72,37 @@ class DonationController extends Controller
     public function newAction(Request $request)
     {
         $serializer = $this->get('jms_serializer');
-        $em = $this->getDoctrine()->getManager();
         $donation = new Donation();
 
-        $message = \Swift_Message::newInstance()
-            ->setContentType("text/html")
-            ->setSubject('IPN tapé')
-            ->setFrom('paulgabriel7@gmail')
-            ->setTo('paulgabriel7@gmail.com')
-            ->setBody('L IPN hook a été tapé ');
-        $this->get('mailer')->send($message);
+        $form = $this->createForm('AppBundle\Form\DonationType', $donation);
+        $form->submit($request->request->all());
 
-        // STEP 1: Read POST data
+        if ($form->isSubmitted() ) {//&& $form->isValid()
+            $data = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($donation);
+            $em->flush();
 
-        // reading posted data from directly from $_POST causes serialization
-        // issues with array data in POST
-        // reading raw POST data from input stream instead.
-        $raw_post_data = file_get_contents('php://input');
-        $raw_post_array = explode('&', $raw_post_data);
-        $myPost = array();
-        foreach ($raw_post_array as $keyval) {
-          $keyval = explode ('=', $keyval);
-          if (count($keyval) == 2)
-             $myPost[$keyval[0]] = urldecode($keyval[1]);
-        }
-        // read the post from PayPal system and add 'cmd'
-        $req = 'cmd=_notify-validate';
-        if(function_exists('get_magic_quotes_gpc')) {
-           $get_magic_quotes_exists = true;
-        }
-        foreach ($myPost as $key => $value) {
-           if($get_magic_quotes_exists == true && get_magic_quotes_gpc() == 1) {
-                $value = urlencode(stripslashes($value));
-           } else {
-                $value = urlencode($value);
-           }
-           $req .= "&$key=$value";
-        }
+            $response = new Response($serializer->serialize(array(
+                'status' => 1,
+                'donation' => $donation), 'json'));
+            $response->headers->set('Content-Type', 'application/json');
 
-        // STEP 2: Post IPN data back to paypal to validate
+            return $response;
+        } else {
 
-        $ch = \curl_init('https://www.paypal.com/cgi-bin/webscr'); // change to [...]sandbox.paypal[...] when using sandbox to test
-        \curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-        \curl_setopt($ch, CURLOPT_POST, 1);
-        \curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-        \curl_setopt($ch, CURLOPT_POSTFIELDS, $req);
-        \curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
-        \curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-        \curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);
-        \curl_setopt($ch, CURLOPT_HTTPHEADER, array('Connection: Close'));
-
-        // In wamp like environments that do not come bundled with root authority certificates,
-        // please download 'cacert.pem' from "http://curl.haxx.se/docs/caextract.html" and set the directory path
-        // of the certificate as shown below.
-        // curl_setopt($ch, CURLOPT_CAINFO, dirname(__FILE__) . '/cacert.pem');
-        if( !($res = curl_exec($ch)) ) {
-            // error_log("Got " . curl_error($ch) . " when processing IPN data");
-            curl_close($ch);
-            exit;
-        }
-        \curl_close($ch);
-
-
-        // STEP 3: Inspect IPN validation result and act accordingly
-
-        if (strcmp ($res, "VERIFIED") == 0) {
-            // check whether the payment_status is Completed
-            // check that txn_id has not been previously processed
-            // check that receiver_email is your Primary PayPal email
-            // check that payment_amount/payment_currency are correct
-            // process payment
-
-            $message = \Swift_Message::newInstance()
-                ->setContentType("text/html")
-                ->setSubject('IPN vérifié')
-                ->setFrom('paulgabriel7@gmail')
-                ->setTo('paulgabriel7@gmail.com')
-                ->setBody('L IPN a été vérifié ');
-            $this->get('mailer')->send($message);
-
-            // assign posted variables to local variables
-            $item_name = $_POST['item_name'];
-            $item_number = $_POST['item_number'];
-            $payment_status = $_POST['payment_status'];
-            if ($_POST['mc_gross'] != NULL)
-                $payment_amount = $_POST['mc_gross'];
-            else
-                $payment_amount = $_POST['mc_gross1'];
-            $payment_currency = $_POST['mc_currency'];
-            $txn_id = $_POST['txn_id'];
-            $receiver_email = $_POST['receiver_email'];
-            $payer_email = $_POST['payer_email'];
-            $custom = $_POST['custom'];
-
-            $sticker = $this->getDoctrine->getRepository('AppBundle:Sticker')->findOneById($custom);
-
-
-            if(!empty($sticker)){
-                $donation->setSticker($custom);
-                $donation->setAmount($payment_amount);
-                $donation->setPaypalTransactionId($payer_email);
-                $em->persist($donation);
-                $em->flush();
-            } else {
-                return new JsonResponse(array(
-                    'status' => 0,
-                    'custom' => 'No Sticker'));
-            }
-
-        } else if (strcmp ($res, "INVALID") == 0) {
-            $message = \Swift_Message::newInstance()
-                ->setContentType("text/html")
-                ->setSubject('IPN refusé')
-                ->setFrom('paulgabriel7@gmail')
-                ->setTo('paulgabriel7@gmail.com')
-                ->setBody('L IPN a été refusé ');
-            $this->get('mailer')->send($message);
-            return new JsonResponse(array(
+            $response = new JsonResponse(array(
                 'status' => 0,
-                'custom' => 'undefined'));
+                'errors' => $form->getErrorsAsString()));
+            return $response;
+
         }
+
+        $response = new JsonResponse(array(
+            'status' => 2,
+            'errors' => $form->getErrorsAsString()));
+
+        return $response;
     }
 
     /**
